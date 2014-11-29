328ff897523eaff2fd8f45a2c3ce024f70b193f2
Kevin Brightwell <kevin.brightwell2@gmail.com>
2014-11-29 12:35:05 -0500

    Fixes case-sensitivity issues causing CMake to fail on different platforms, this should right it.

--- share/QHttpServerConfig.cmake
+++ share/QHttpServerConfig.cmake
@@ -1,21 +1,21 @@
-# - Try to find qhttpserver
+# - Try to find QHttpServer
 # Once done this will define
-#  QHTTPSERVER_FOUND - System has qhttpserver
-#  QHTTPSERVER_INCLUDE_DIRS - The qhttpserver include directories
-#  QHTTPSERVER_LIBRARIES - The libraries needed to use qhttpserver
-#  QHTTPSERVER_DEFINITIONS - Compiler switches required for using qhttpserver
+#  QHTTPSERVER_FOUND - System has QHttpServer
+#  QHTTPSERVER_INCLUDE_DIRS - The QHttpServer include directories
+#  QHTTPSERVER_LIBRARIES - The libraries needed to use QHttpServer
+#  QHTTPSERVER_DEFINITIONS - Compiler switches required for using QHttpServer
 
 find_package(PkgConfig)
 
-pkg_check_modules(PC_QHTTPSERVER QUIET @qhttpserver_LIB_NAME@-@qhttpserver_LIB_VERSION@)
+pkg_check_modules(PC_QHTTPSERVER QUIET @QHttpServer_LIB_NAME@-@QHttpServer_LIB_VERSION@)
 
 set(QHTTPSERVER_DEFINITIONS ${PC_QHTTPSERVER_CFLAGS_OTHER})
 
-find_path(QHTTPSERVER_INCLUDE_DIR @qhttpserver_LIB_NAME@/qhttpserver.h
+find_path(QHTTPSERVER_INCLUDE_DIR @QHttpServer_LIB_NAME@/qhttpserver.h
           HINTS ${PC_QHTTPSERVER_INCLUDEDIR} ${PC_QHTTPSERVER_INCLUDE_DIRS}
-          PATH_SUFFIXES qhttpserver )
+          PATH_SUFFIXES QHttpServer )
 
-find_library(QHTTPSERVER_LIBRARY NAMES @qhttpserver_LIB_NAME@ lib@qhttpserver_LIB_NAME@
+find_library(QHTTPSERVER_LIBRARY NAMES @QHttpServer_LIB_NAME@ lib@QHttpServer_LIB_NAME@
              HINTS ${PC_QHTTPSERVER_LIBDIR} ${PC_QHTTPSERVER_LIBRARY_DIRS} )
 
 set(QHTTPSERVER_LIBRARIES ${QHTTPSERVER_LIBRARY} )
@@ -25,7 +25,7 @@ include(FindPackageHandleStandardArgs)
 
 # handle the QUIETLY and REQUIRED arguments and set QHTTPSERVER_FOUND to TRUE
 # if all listed variables are TRUE
-find_package_handle_standard_args(qhttpserver  DEFAULT_MSG
+find_package_handle_standard_args(QHttpServer  DEFAULT_MSG
                                   QHTTPSERVER_LIBRARY QHTTPSERVER_INCLUDE_DIR)
 
 mark_as_advanced(QHTTPSERVER_INCLUDE_DIR QHTTPSERVER_LIBRARY )
